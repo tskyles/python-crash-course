a665861ab560a85f72e69939b71d53624f77c32f
travis skyles <travis.skyles@gmail.com>
2019-12-31 14:53:04 -0800

    handle methods

---
+++ server.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler
+class Server(BaseHTTPRequestHandler)
+  def do_HEAD(self):
+    return
+  def do_POST(self):
+    return
+  def do_GET(self):
+    return
+  def handle_http(self):
+    return
+  def respond(self):
+    return
