25127f1042495e6b00e851c244cdad560fd1218f
travis skyles <travis.skyles@gmail.com>
2020-01-02 12:55:35 -0800

    created staticHandler

---
+++ response/staticHandler.py
@@ -0,0 +1,11 @@
+from response.requestHandler import RequestHandler
+
+class StaticHandler(RequestHandler):
+  def __init__(self):
+    return
+  
+  def find(self, file_path):
+    return
+
+  def setContentType(self, ext):
+    return
